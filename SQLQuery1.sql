2f74ec9cfb096a9247f2afa70e9538d7f69dc48f
Sahinshf <shahinrsha@code.edu.az>
2023-04-17 23:21:54 +0400

    Task is updated

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -3,58 +3,101 @@ CREATE DATABASE CinemaDb
 USE CinemaDb
 
 --Movie-----------------------------------------------------------------
-CREATE TABLE Movie(
+CREATE TABLE Movies(
 	movieId int primary key identity,
 	filmName nvarchar(100) NOT NULL,
-	filmGenre nvarchar(100) NOT NULL
 );
 
-INSERT INTO Movie
-VALUES ('Schindlers List','Drama History Biography'),
-('Pulp Fiction','Drama Crime'),
-('Forrest Gump','Drama Romance'),
-('Fight Club','Drama'),
-('3 Idiots','Comedy Drama'),
-('The Pianist','Drama Music Biography')
+INSERT INTO Movies
+VALUES ('Schindlers List'),
+('Pulp Fiction'),
+('Forrest Gump'),
+('Fight Club'),
+('3 Idiots'),
+('The Pianist')
 
-
---Theatre-----------------------------------------------------------------
-CREATE TABLE Theatre(
-	cinemaId int primary key identity,
-	cinemaName nvarchar(100) NOT NULL,
+--Genre------------------------------------------------------------------
+CREATE TABLE Genres (
+  genreId int primary key identity,
+  genreName nvarchar(100) NOT NULL
 );
 
-INSERT INTO Theatre
-VALUES ('Alamo Drafthouse in Austin' ),
-('Cineteca Matadero in Madrid'),
-('Cine-de Chef in Kathmandu')
+INSERT INTO Genres
+VALUES ('Drama') , ('Romance') , ('Music') , ('Biography') , ('Comedy') , ('Crime') , ('History')
+
+
+--Movies Genre------------------------------------------------------------
+
+CREATE TABLE MoviesGenres (
+  movieID int NOT NULL,
+  genreID int NOT NULL,
+  constraint filmGenre_pk primary key (movieID, genreID),
+  foreign key (movieID) references Movies(movieId),
+  foreign key (genreID) references Genres(genreId)
+);
+
+INSERT INTO MoviesGenres
+VALUES (1,1) , (1,4) , (1,7) , (2,1) , (2,6) , (3,1) , (3,2) , (4,1) , (5,1) , (5,5) , (6,1) , (6,3) , (6,4)
+
+
+--Actor------------------------------------------------------------------
+CREATE TABLE Actors (
+  actorId int primary key identity,
+  fullName nvarchar(100) NOT NULL,
+);
+
+INSERT INTO Actors
+VALUES ('Liam Neeson') , ('Ralph Fiennes') , ('John Travolta') ,  ('Tom Hanks') , ('Brad Pitt') , ('Edward Norton') , ('Aamin Khan') , ('Adrien Brody')
+
+
+--Actor Movies------------------------------------------------------------------
+CREATE TABLE MoviesActors (
+  movieID int,
+  actorID int,
+  constraint filmActor_pk primary key (movieID, actorID),
+  foreign key (movieID) references Movies(movieId),
+  foreign key (actorID) references Actors(actorId)
+);
+
+INSERT INTO MoviesActors
+VALUES (1,1) , (1,2) ,  (2,3) , (3,4) ,  (4,5) , (4,6) , (5,7) , (6,8) 
+
+
+--Hall-----------------------------------------------------------------
+CREATE TABLE Halls (
+  hallId int primary key identity,
+  hallName nvarchar(100),
+);
+
+INSERT INTO Halls
+VALUES ('HALL 1') , ('HALL 2') , ('HALL 3') , ('HALL 4') , ('HALL 5') , ('HALL 6') , ('HALL 7')
 
 --ShowTime-----------------------------------------------------------------
-CREATE TABLE ShowTime(
+CREATE TABLE ShowTimes(
 	showTimeId int primary key identity,
-	movieID int foreign key references Movie(movieId) NOT NULL,
-	cinemaID int foreign key references Theatre(cinemaId) NOT NULL,
+	movieID int foreign key references Movies(movieId) NOT NULL,
+	hallID int foreign key references Halls(hallId) NOT NULL,
 	startTime datetime2 NOT NULL,
 	endTime datetime2 NOT NULL
 );
 
-INSERT INTO ShowTime
+INSERT INTO ShowTimes
 VALUES ( 2 , 3 , '2022-06-23 07:30:20' , '2022-06-23 09:30:00' ),
 ( 5 , 1 , '2023-07-23 17:30:20' , '2023-07-23 17:30:00' ),
  ( 1 , 2 , '2023-07-23 17:30:20' , '2023-07-23 17:30:00' ),
- ( 3 , 3 , '2023-08-23 18:30:20' , '2023-08-23 18:30:00' ),
- ( 5 , 1 , '2023-07-24 19:30:20' , '2023-07-24 19:30:00' ),
- ( 4 , 2 , '2023-07-23 20:30:20' , '2023-07-23 20:30:00' ),
- ( 6 , 3 , '2022-06-23 07:30:20' , '2022-06-23 09:30:00' )
+ ( 3 , 5 , '2023-08-23 18:30:20' , '2023-08-23 18:30:00' ),
+ ( 5 , 5 , '2023-07-24 19:30:20' , '2023-07-24 19:30:00' ),
+ ( 4 , 7 , '2023-07-23 20:30:20' , '2023-07-23 20:30:00' ),
+ ( 6 , 6 , '2022-06-23 07:30:20' , '2022-06-23 09:30:00' )
 
 
 --Customer-----------------------------------------------------------------
-CREATE TABLE Customer(
+CREATE TABLE Customers(
 	customerId int primary key identity,
 	customerName nvarchar(100) NOT NULL,
 	customerSurname nvarchar(100) NOT NULL
 );
-INSERT INTO Customer
+INSERT INTO Customers
 VALUES ( 'Shahin' , 'Sherifzade' ),
 ( 'Tural' , 'Cavadzade' ),
 ( 'Ayxan' , 'Bayramli' ),
@@ -63,16 +106,16 @@ VALUES ( 'Shahin' , 'Sherifzade' ),
 ( 'Seid' , 'Babazade' )
 
 --Ticket-----------------------------------------------------------------
-CREATE TABLE Ticket(
+CREATE TABLE Tickets(
 	ticketId int primary key identity,
-	showTimeID int foreign key references ShowTime(showTimeId) NOT NULL,
-	customerID int foreign key references Customer(customerId) ,
+	showTimeID int foreign key references ShowTimes(showTimeId) NOT NULL,
+	customerID int foreign key references Customers(customerId) ,
 	Hallrow int check(Hallrow > = 0) NOT NULL,
 	Hallcol int check(Hallcol >= 0) NOT NULL,
 	price decimal(5,2) CHECK (price >= 0) NOT NULL,
 	seatStatus nvarchar(100) DEFAULT 'available' NOT NULL
 );
-INSERT INTO Ticket
+INSERT INTO Tickets
 VALUES ( 2 , 3, 5 , 5,  9.99 , 'reserved'),
 ( 1 , 4 , 5 , 2, 10.00, 'reserved'),
 ( 4 , 2 , 7 , 3, 12.33, 'reserved'),
@@ -80,7 +123,7 @@ VALUES ( 2 , 3, 5 , 5,  9.99 , 'reserved'),
 ( 6 , 6 , 9 , 1, 5.00, 'reserved'),
 ( 3 , 5 , 4 , 8, 7.80, 'reserved')
 
-INSERT INTO Ticket (showTimeID,Hallrow,Hallcol,price)
+INSERT INTO Tickets (showTimeID,Hallrow,Hallcol,price)
 VALUES ( 1 , 12 , 3,  9.99 ),
 ( 5 , 14 , 6,  12.50 ),
 ( 3 , 9 , 9,  4.99 ),
@@ -91,14 +134,14 @@ VALUES ( 1 , 12 , 3,  9.99 ),
 
 CREATE VIEW [RESERVED_TICKETS]
 AS
-SELECT c.customerName , c.customerSurname , m.filmName , m.filmGenre , t.cinemaName , s.startTime , s.endTime , ti.Hallcol , ti.Hallrow  FROM ShowTime s
-INNER JOIN Theatre t
-ON s.cinemaID = t.cinemaId
-INNER JOIN Movie m
+SELECT c.customerName 'Name' , c.customerSurname 'Surname', m.filmName 'Movie', h.hallName 'Hall', s.startTime 'Start', s.endTime 'End' , ti.Hallcol 'Seat - Column' , ti.Hallrow 'Seat - Row' FROM ShowTimes s
+INNER JOIN Halls h
+ON s.hallID = h.hallId
+INNER JOIN Movies m
 ON s.movieID = m.movieId
-INNER JOIN Ticket ti
+INNER JOIN Tickets ti
 ON s.showTimeId = ti.showTimeID
-INNER JOIN Customer c
+INNER JOIN Customers c
 ON  c.customerId= ti.customerID
 
 SELECT * FROM [RESERVED_TICKETS]
@@ -107,12 +150,12 @@ SELECT * FROM [RESERVED_TICKETS]
 
 CREATE VIEW [AVAILABLE_TICKETS]
 AS
-SELECT  m.filmName , m.filmGenre , t.cinemaName , s.startTime , s.endTime , ti.Hallcol , ti.Hallrow , ti.seatStatus  FROM ShowTime s
-INNER JOIN Theatre t
-ON s.cinemaID = t.cinemaId
-INNER JOIN Movie m
+SELECT  m.filmName 'Movie' , h.hallName 'Hall', s.startTime 'Start', s.endTime 'End' , ti.Hallcol 'Seat - Column', ti.Hallrow'Seat - Row' , ti.seatStatus 'Seat - Status' , ti.price 'Price' FROM ShowTimes s
+INNER JOIN Halls h
+ON s.hallID = h.hallId
+INNER JOIN Movies m
 ON s.movieID = m.movieId
-INNER JOIN Ticket ti
+INNER JOIN Tickets ti
 ON s.showTimeId = ti.showTimeID
 WHERE ti.customerID IS NULL  
 
@@ -121,3 +164,29 @@ SELECT * FROM [AVAILABLE_TICKETS]
 
 --INNER JOIN Customer c
 --ON  c.customerId= ti.customerID
+
+--VALUES ('Schindlers List','Drama History Biography'),
+--('Pulp Fiction','Drama Crime'),
+--('Forrest Gump','Drama Romance'),
+--('Fight Club','Drama'),
+--('3 Idiots','Comedy Drama'),
+--('The Pianist','Drama Music Biography')
+
+
+--Theatre-----------------------------------------------------------------
+
+--CREATE TABLE Theatres(
+--	cinemaId int primary key identity,
+--	cinemaName nvarchar(100) NOT NULL,
+--);
+
+--INSERT INTO Theatres
+--VALUES ('Alamo Drafthouse in Austin' ),
+--('Cineteca Matadero in Madrid'),
+--('Cine-de Chef in Kathmandu')
+
+--SELECT m.filmName , g.genreName FROM MoviesGenres ms
+--join Genres g 
+--ON g.genreId = ms.genreID
+--join Movies m
+--ON m.movieId = ms.movieID
